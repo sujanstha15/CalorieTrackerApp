98698948aef2f9a7e40b7d6b8acc97a0dfec8311
Sujan Shrestha <raju40rr@gmail.com>
2025-08-06 00:19:54 -0500

    first commit

---
+++ src/main/java/com/calorietrackerapp/CalorieTrackerappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.calorietrackerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieTrackerappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieTrackerappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calorietrackerapp/CalorieTrackerappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calorietrackerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieTrackerappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
